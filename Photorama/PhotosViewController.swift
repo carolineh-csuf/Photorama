94799830a4c6b7d90c24f11d1e0d4d03b8505531
carolineh-csuf <carolineh@csu.fullerton.edu>
2023-04-10 11:18:51 -0700

    to be done

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by csuftitan on 4/10/23.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+//    required init?(coder aDecoder: NSCoder) {
+//       // self.username = "Anonymous"
+//        super.init(coder: aDecoder)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
